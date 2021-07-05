f287742f06beede3db88bcbaa290d72170f5febb
takuma375 <takuma-shimizu-10174@keio.jp>
2021-07-05 22:34:36 +0900

    AOJ ITP Topic01 X Cubic

---
+++ AOJ_introductionToProgramming/Topic01/xCubic.cpp
@@ -0,0 +1,11 @@
+// １つの整数 x を読み込んで、x の３乗を計算し結果を出力するプログラムを作成して下さい。
+#include <iostream>
+using namespace std;
+
+int main() {
+    int X;
+    cin >> X;
+
+    cout << X*X*X << endl;
+    return 0;
+}
