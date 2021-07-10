679883754e72f23a69a7fbf8cca277b5da63f7f3
takuma375 <takuma-shimizu-10174@keio.jp>
2021-07-10 22:55:05 +0900

    AOJ ITP Topic02 Range(.py)

---
+++ AOJ_introductionToProgramming/Topic02/range.py
@@ -0,0 +1,8 @@
+# ３つの整数a, b, cを読み込み、それらが a < b < cの条件を満たすならば"Yes"を、満たさないならば"No"を出力するプログラムを作成して下さい。
+
+a, b, c = list(map(int, input().split()))
+
+if a<b<c:
+    print("Yes")
+else:
+    print("No")
